bb7c7f62b6e5df54b53aae816f57223d8e68dba2
umagloire <umagloire99@gmail.com>
2022-07-28 10:05:09 +0100

    adding a config settings file that contains static configuration from the .env
    Signed-off-by: umagloire <umagloire99@gmail.com>

---
+++ config/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+    |--------------------------------------------------------------------------
+    | platform settings
+    |--------------------------------------------------------------------------
+    | contains all static configurations from the .env
+    |
+    */
+return [
+    'user_password' => env('STATIC_USER_PASSWORD', 'laravel')
+];
