859e556a08520cdc7dcc82c856209cc78203591b
umagloire <umagloire99@gmail.com>
2022-07-28 10:28:37 +0100

    adding an endpoint to get user detail
    Signed-off-by: umagloire <umagloire99@gmail.com>

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * show user detail
+     * @param $id
+     * @return Application|Factory|View|JsonResponse
+     */
+    public function show($id)
+    {
+        $user = User::find($id);
+        if ($user) {
+            return view('users.view', compact('user'));
+        } else {
+            return response()->json("No such user ($id)", 404);
+        }
+    }
 }
